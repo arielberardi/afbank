175093b7aeb6b8352a3c37fae092faca26cbd882
Ariel Berardi <ariel.berardi95@gmail.com>
2024-08-03 08:32:24 +0100

    Fix creation action not broadcasting to turbo stream

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,4 +5,8 @@ class Contact < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: { scope: :user_id }
   validates :account_id, uniqueness: { scope: :user_id }
+
+  after_create_commit -> { broadcast_append_later_to 'contacts' }
+  after_update_commit -> { broadcast_replace_later_to 'contacts' }
+  after_destroy_commit -> { broadcast_remove_to 'contacts' }
 end
